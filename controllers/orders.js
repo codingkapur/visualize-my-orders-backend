a73c255a65228d804266ca9cb17756517f3a6ec4
Raghav <codingkapur@gmail.com>
2021-10-27 17:42:10 +0530

    implement all CRUD operations and routes, tested with POSTMAN

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,26 +1,68 @@
 const Order = require("../models/order");
 
 const getAllOrders = async (req, res) => {
-    try {
-        const orders = await Order.find({});
-        if(!orders){
-            return res.status(404).send("No Orders Found")
-        }
-        res.status(200).json(orders);
-    } catch (error) {
-        res.status(500).send(error.message)
+  try {
+    const orders = await Order.find({});
+    if (!orders) {
+      return res.status(404).send("No Orders Found");
     }
-}
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 const createOrder = async (req, res) => {
-    try{
-        const order = await Order.create(req.body);
-        res.status(200).send(order);
+  try {
+    const order = await Order.create(req.body);
+    res.status(200).send(order);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+const getOrder = async (req, res) => {
+    try {
+      const { id: orderID } = req.params;
+      const order = await Order.findOne({ _id: orderID });
+      if(!order){
+        return res.status(404).json({msg: "No such order found!"})
+      }
+      res.status(200).json(order)
     } catch (error) {
-        res.status(500).send(error.message);
+      res.status(500).send(error.message);
     }
-}
+  };
+const deleteOrder = async (req, res) => {
+  try {
+      const {id: orderID } = req.params; 
+      const order = await Order.findByIdAndDelete({_id : orderID});
+      if (!order) {
+        return res.status(500).send("This order does not exist foo!");
+      }
+      res.status(200).send("Order Deleted Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateOrder = async (req, res) => {
+    try {
+      const { id: orderID } = req.params;
+      const order = await Order.findOneAndUpdate({ _id: orderID }, req.body, {
+        new:true,
+        runValidators:true
+      });
+      if(!order){
+        return res.status(404).json({msg: "No such order found!"})
+      }
+      res.status(200).json(order)
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
 
 module.exports = {
-    getAllOrders,
-    createOrder,
-}
+  getAllOrders,
+  createOrder,
+  getOrder,
+  deleteOrder,
+  updateOrder,
+};
--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const {
     getAllOrders,
     createOrder,
+    getOrder,
+    deleteOrder,
+    updateOrder
 } = require("../controllers/orders");
 
 router.route("/").get(getAllOrders).post(createOrder);
-// router.route("/:id").get().patch().delete();
+router.route("/:id").get(getOrder).patch(updateOrder).delete(deleteOrder);
 
 module.exports = router;
