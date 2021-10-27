ce7f559420836260ac220e27d4718a7160105a52
Raghav <codingkapur@gmail.com>
2021-10-27 16:11:36 +0530

    improved schema for orders with default values and restricted options

--- models/order.js
+++ models/order.js
@@ -12,24 +12,42 @@ const OrdersSchema = new mongoose.Schema({
   },
   address: {
     type: String,
+    required: [true, "Must provide an address"],
   },
   phone: {
     type: Number,
+    required: [true, "Must provide a number"],
     maxlength: 10,
     minlength: 10,
   },
   time: {
     type: Date,
+    default: Date.now(),
   },
   item: {
     type: String,
+    required: [true, "Must provide an item"],
+    enum : ["T-Shirt", "Shorts", "Pants", "Track Pants", "Hoodie", "Sweatshirt", "Sneakers"],
+
   },
   source: {
     type: String,
+    required: [true, "Must provide a source"],
+    enum : ["Website", "Manual"],
+
   },
-  status: {
+  delivery_status: {
     type: String,
+    required: [true, "Must provide delivery status"],
+    enum : ["Not Delivered", "Delivered"],
+
   },
+  payment_status : {
+    type: String,
+    required: [true, "Must provide payment status"],
+    enum : ["Paid", "On Delivery"],
+    
+  }
 });
 
 module.exports = mongoose.model("Order", OrdersSchema);
